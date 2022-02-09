dc1983907b9cbb8a14b006ea837ed321a6ae5235
Valentinas <vkornijenka@gmail.com>
2022-02-09 16:08:37 +1000

    added function addBookToLibrary()

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   this.info = function () {
-    return title, author, pages, read;
+    return [title, author, pages, read];
   };
 }
 
-// function addBookToLibrary() {
-//   // prompts user for book
-//   // asks for title, author, pages and read
-//   // calls Book() and puts item into myLibrary
-// }
+function addBookToLibrary() {
+  let title = prompt("Title");
+  let author = prompt("Author");
+  let pages = prompt("Pages");
+  let read = prompt("Read?");
+  if (title != null && read != null) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+  }
+}
