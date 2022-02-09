0378c8eadd57b086122893abe6741d146db4a693
Valentinas <vkornijenka@gmail.com>
2022-02-09 17:27:20 +1000

    added function createLibraryCard()

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ function addBookToLibrary() {
     myLibrary.push(newBook);
   }
 }
+
+function createLibraryCard(title, author, pages, read) {
+  const bookCard = document.createElement("div");
+  const bookTitle = document.createElement("h3");
+  const bookAuthor = document.createElement("h4");
+  const bookPages = document.createElement("p");
+  const bookRead = document.createElement("p");
+  bookTitle.innerText = `Title: ${title}`;
+  bookAuthor.innerText = `Author: ${author}`;
+  bookPages.innerText = `Pages: ${pages}`;
+  bookRead.innerText = `Has it been read? - ${read}`;
+  bookCard.classList.add("bookCard", "card-contend");
+  bookTitle.classList.add("bookTitle", "card-contend");
+  bookAuthor.classList.add("bookAuthor", "card-contend");
+  bookPages.classList.add("bookPages", "card-contend");
+  bookRead.classList.add("bookRead", "card-contend");
+  bookCard.appendChild(bookTitle);
+  bookCard.appendChild(bookAuthor);
+  bookCard.appendChild(bookPages);
+  bookCard.appendChild(bookRead);
+  document.getElementById("library").appendChild(bookCard);
+}
+
+//make function to loop through array and create each book;
