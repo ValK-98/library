3bdd6c5d5070e9889771d0702b85f01d8ac77308
Valentinas <vkornijenka@gmail.com>
2022-02-09 15:44:53 +1000

    function draft

---
+++ script.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+
+function Book(){
+    // constructor
+    // title
+    // author 
+    // number of pages
+    // read? true/false
+    // return values 
+}
+
+function addBookToLibrary(){
+    // prompts user for book 
+    // asks for title, author, pages and read 
+    // calls Book() and puts item into myLibrary
+}
