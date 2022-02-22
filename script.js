457c32047c132f8e065b6dc2dd3dc7d002fe607d
Valentinas <vkornijenka@gmail.com>
2022-02-22 18:34:08 +1000

    refactoring to class as per TOP

--- script.js
+++ script.js
@@ -12,15 +12,27 @@ closeFormButton.addEventListener("click", closeForm);
 submitBook.addEventListener("click", addBookToLibrary);
 checkbox.addEventListener("onClick", toggleCheckbox);
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.exists = -0;
-  this.returnArr = function () {
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+//   this.exists = -0;
+//   this.returnArr = function () {
+//     return [title, author, pages, read];
+//   };
+// }
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  returnArr() {
     return [title, author, pages, read];
-  };
+  }
 }
 
 function addBookToLibrary() {
